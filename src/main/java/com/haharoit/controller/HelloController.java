19ad0a6c822d5bb9d43f896ca5ed51ef10ca9e32
haharoit <163920489@qq.com>
2018-05-29 17:14:11 +0800

    add thymeleaf

--- src/main/java/com/haharoit/controller/HelloController.java
+++ src/main/java/com/haharoit/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.haharoit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String index(){
+    public String hello(){
         return "Hello World";
     }
+
+    @RequestMapping("/")
+    public String index(ModelMap map){
+        map.addAttribute("host", "http//www.haharoit.com");
+
+        return "index";
+    }
+
+
 }
