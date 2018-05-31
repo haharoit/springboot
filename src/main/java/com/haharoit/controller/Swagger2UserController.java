ac979f695a602ed0fa583eccd68215c2a0dbcc89
haharoit <163920489@qq.com>
2018-05-31 14:00:02 +0800

    Update ApilmplicitParam paramType field for swagger

--- src/main/java/com/haharoit/controller/Swagger2UserController.java
+++ src/main/java/com/haharoit/controller/Swagger2UserController.java
@@ -30,15 +30,19 @@ public class Swagger2UserController {
     }
 
     @ApiOperation(value="创建用户", notes="根据User对象创建用户")
-    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
-    @RequestMapping(value="/", method= RequestMethod.POST)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true,paramType = "query" , dataType = "Long"),
+            @ApiImplicitParam(name = "name", value = "user名称", required = true, paramType = "query" , dataType = "String"),
+            @ApiImplicitParam(name = "age", value = "user年龄", required = true, paramType = "query" , dataType = "Long")
+    })    @RequestMapping(value="/", method= RequestMethod.POST)
     public String postUser(@ModelAttribute User user) throws Exception{
         users.put(user.getId(), user);
         return "success";
     }
 
-    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    //paramType 有五个可选值 ： path, query, body, header, form
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息",httpMethod = "GET")
+    @ApiImplicitParam(name = "id", value = "用户ID",paramType = "path" , required = true, dataType = "Long")
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
         return users.get(id);
@@ -46,7 +50,7 @@ public class Swagger2UserController {
 
     @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true,paramType = "path" , dataType = "Long"),
             @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
     })
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
@@ -59,7 +63,7 @@ public class Swagger2UserController {
     }
 
     @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @ApiImplicitParam(name = "id", value = "用户ID",paramType = "path" , required = true, dataType = "Long")
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public String deleteUser(@PathVariable Long id) {
         users.remove(id);
